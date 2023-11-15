bd71045c6400ca319b3ea3c549233eca6c48a7d5
Ivan Martin Lomoro <ivanmartin_95@hotmail.com>
2023-11-15 13:24:54 +0100

    [Fix] : bugs types

--- backend/src/db/client.ts
+++ backend/src/db/client.ts
@@ -1,15 +1,10 @@
 import { PrismaClient as MongoClient } from "../../prisma/generated/mongodb_client";
 import { PrismaClient as PostgresClient } from "../../prisma/generated/postgresql_client";
-import { Prisma } from "@prisma/client"
-import { DefaultArgs } from "@prisma/client/runtime/library";
 
 export const DATA_SOURCE = process.env.DATA_SOURCE ?? "mongo"
 
-type ClientMongo = MongoClient<Prisma.PrismaClientOptions, never, DefaultArgs>
-type ClientPostgres = PostgresClient<Prisma.PrismaClientOptions, never, DefaultArgs>
-
-export const mongoClient: ClientMongo = new MongoClient();
-export const postgresClient: ClientPostgres = new PostgresClient();
+export const mongoClient = new MongoClient();
+export const postgresClient = new PostgresClient();
 
 export let prismaClient: any
 
