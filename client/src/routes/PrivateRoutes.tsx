cf93d75590a1b8316837b0f9ed0242dd0b8a2be8
Ivan Martin Lomoro <ivanmartin_95@hotmail.com>
2023-11-16 12:19:18 +0100

    [Feat] : Add private routes

---
+++ client/src/routes/PrivateRoutes.tsx
@@ -0,0 +1,25 @@
+import { FC, ReactNode } from "react"
+import { useAuth0 } from '@auth0/auth0-react'
+import { Navigate } from "react-router-dom"
+
+
+type PrivateRoutesProps = {
+  children: ReactNode
+}
+
+const PrivateRoutes: FC<PrivateRoutesProps> = ({children}) => {
+
+  const { user, isAuthenticated, isLoading} = useAuth0();
+  console.log("User:",user)
+  console.log("isAuthenticated:",isAuthenticated)
+
+  if (isLoading) {
+    return <div>Loading...</div>
+}
+
+  return (
+    isAuthenticated ? children: <Navigate to="/" replace={true}/>
+  )
+}
+
+export default PrivateRoutes
--- client/src/routes/RouterPaths.routes.tsx
+++ client/src/routes/RouterPaths.routes.tsx
@@ -1,13 +1,16 @@
-import { Route, Routes} from 'react-router-dom'
+import { Route, Routes } from 'react-router-dom'
 import Home from '../Page/Home'
 import LoginPage from '../Page/LoginPage'
+import PrivateRoutes from './PrivateRoutes'
 
 const RouterPaths = () => {
   return (
     <>
       <Routes>
         <Route path="/" element={<LoginPage />} />
-        <Route path="/home" element={<Home />} />
+            <Route path="/home" element={
+              <PrivateRoutes><Home /></PrivateRoutes>
+            } />
       </Routes>
     </>
   )
