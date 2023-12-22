2618d30659eeb9e36d64db760f35c350e13b5a62
Ivan Martin Lomoro <ivanmartin_95@hotmail.com>
2023-12-22 18:04:36 +0100

    [Fix] : Bug with create or login user

--- backend/src/controllers/user.controllers.ts
+++ backend/src/controllers/user.controllers.ts
@@ -16,7 +16,6 @@ export const getAllUsers = async (req: Request, res: Response) => {
 };
 
 export const createUserOrLogin = async (req: Request, res: Response) => {
-    console.log("Esta corriendo el back")
     try {
         const { name, email } = req.body;
 
